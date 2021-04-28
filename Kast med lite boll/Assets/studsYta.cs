abf7199ced4ff4a39e21c444eb67580d1f198540
Jonas96Lundin <62259168+Jonas96Lundin@users.noreply.github.com>
2021-04-28 16:40:30 +0200

    A2

--- Kast med lite boll/Assets/studsYta.cs
+++ Kast med lite boll/Assets/studsYta.cs
@@ -1,8 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class studsYta : MonoBehaviour
 {
     public float studskoefficient = 1;
+    public float friktionskoefficient = 1;
+
+    [SerializeField]
+    string type;
+    [SerializeField]
+    Text friktionskoefficientText;
+    [SerializeField]
+    Text studskoefficientText;
+    private void Start()
+    {
+        friktionskoefficientText.text += "\n" + type + " Friktionskoefficient " + friktionskoefficient;
+        studskoefficientText.text += "\n" + type + " Studskoefficient " + studskoefficient;
+    }
 }
